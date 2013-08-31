5bcbe56a761a119fa55ce1255a5cc0f3e754eae7
huoshi5151 <wt371@qq.com>
2013-08-31 14:15:34 +0800

    array_pop从数组结尾删掉一个元素

---
+++ array_pop.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+</body>
+</html>
+<?php 
+$num=Array (  10,  9 , 8, 7,  6,  5,  4,  3 ,2 , 1 );
+array_pop($num);
+print_r($num);
+
+ ?>
