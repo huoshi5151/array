dcc2b7115eba1ebc544637ce55cbe7f039816ba6
huoshi5151 <wt371@qq.com>
2013-08-31 13:42:31 +0800

    shuffle随机排列数组元素
    随机排列数组元素

---
+++ shuffle.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+</body>
+</html>
+<?php 
+$pictures=array('1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg');
+shuffle($pictures);
+for ($i=0; $i < 3; $i++) { 
+	echo "<img src=\"$pictures[$i]\">";
+}
+ ?>
