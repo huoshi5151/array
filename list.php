9e232946fa6b7bde43fd7dd182106a5bec9231a9
huoshi5151 <wt371@qq.com>
2013-08-31 10:21:57 +0800

    each()处理过的函数在同一脚本中再次使用，需要reset($arr)函数重置一下数组

--- list.php
+++ list.php
@@ -1,10 +1,18 @@
 <?php 
 /**
  * 注意一个是$price一个是$prices如果两个变量冲突，只会出现一个结果，不会将数组全部循环出来
+ * 如果在当前脚本多次使用该数组，下次使用前应该reset($prices)，因为each()函数会记录当前数组元素
  * @var array
  */
 $prices=array('qiyou'=>100,'lunzi'=>200,'zhoucheng'=>300);
 while (list($product,$price) = each($prices)) {
 	echo "$product - $price<br>";
 }
+
+// print_r($prices);
+
+foreach ($prices as $value) {
+	echo $value.'<br />';
+}
+
  ?>
